8b71672a3268d825684f5048cd7d10b10319c1d9
paras <paras@192.168.1.165>
2026-05-27 08:56:46 +0200

    Lo hablado por dc

--- src/main/java/org/sopra/rogueguild/quest/Quest.java
+++ src/main/java/org/sopra/rogueguild/quest/Quest.java
@@ -25,7 +25,6 @@ public class Quest {
 		this.requiredItems = requiredItems;
 	}
 
-
 	public boolean checkRequirement(Player p) {
 		boolean checksRequirement = false;
 		List<Item> inventory = p.getInventory();
